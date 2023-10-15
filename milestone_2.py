aeb38f265f298f4dcb1e2fa10f54dbd082ce420b
plainsman <yiannissanozidis@hotmail.co.uk>
2023-10-15 15:33:22 +0100

    File has been cleaned up.

--- milestone_2.py
+++ milestone_2.py
@@ -1,9 +1,14 @@
 import random
 
+# Describes a list of fruit and chooses one at random.
+
 word_list = ["Mango", "Strawberry", "Peach", "Pumpkin", "Chocolate"]
+
 print(word_list)
 
 word = random.choice(word_list)
+
+# Asks user to input a letter and tells them if valid choice.
 
 guess = input("Please enter a single letter:")
 
@@ -11,5 +16,8 @@ if len(guess) == 1 and guess in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstu
     print("Good Guess!")
 else:
     print("Ooops! That is not a valid input.")
+
+# Print random choice of fruit from earlier.
+
 print(word)
 
