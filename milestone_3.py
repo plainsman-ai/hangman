9ac52dd20d4a97168760ca63ec97d5504db5f3e8
plainsman <yiannissanozidis@hotmail.co.uk>
2023-10-15 16:05:20 +0100

    Changed file to have more abstraction. Also runs with different word each time.

--- milestone_3.py
+++ milestone_3.py
@@ -3,15 +3,22 @@ import random
 word_list = ["Mango", "Strawberry", "Peach", "Pumpkin", "Chocolate"]
 
 word = random.choice(word_list)
-    
-while True:
-    guess = input("Guess a letter: ")
-    if guess.isalpha() and len(guess) == 1:
-        break
-    else:
-        print("Invalid letter. Please enter a single alphabetical character.")
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def check_guess(guess):
+    lower_guess = guess.lower()
+    lower_word = word.lower()
+    if lower_guess in lower_word:
+        print(f"Good guess! {lower_guess} is in the word")
+    else:
+        print(f"Sorry, {lower_guess} is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter: ")
+        if guess.isalpha() and len(guess) == 1:
+            break
+        else:
+            print("Invalid letter. Please enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()
