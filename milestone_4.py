204631a39df980a7e4e6484f1c04ff167115b256
plainsman <yiannissanozidis@hotmail.co.uk>
2023-10-16 15:16:30 +0100

    File now checks if letter is in word.

--- milestone_4.py
+++ milestone_4.py
@@ -7,15 +7,31 @@ class Hangman:
         # the number of lives the player starts with.
         self.num_lives = num_lives
         # the word to be guessed
-        self.word = random.choice(word_list)
+        self.word = random.choice(word_list).lower()
         # list of backspace strings symbolising empty letters
         self.word_guessed = list("_" for letter in range(len(self.word)))
         # the number or unique letters in the word which haven't been guessed
-        self.num_letters = [letter for letter in self.word]
+        self.num_letters = {letter for letter in self.word}
         # list of letters which have already been guessed, starting with 0
         self.list_of_guesses = []
     
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+    
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter: ")
+            guess = guess.lower()
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+
 test_hangman = Hangman(["Apple", "Strawberry"])
-print(test_hangman.word_list)
-print(test_hangman.num_lives)
-print(test_hangman.word_guessed)
+test_hangman.ask_for_input()
